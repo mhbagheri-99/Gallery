a05a4af97f71875bb8bfd57fce95d8bd6099b69f
mhbagheri-99 <mhbagheri3@gmail.com>
2024-05-03 20:18:05 +0200

    modify upload button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,10 @@ import { ClerkProvider } from '@clerk/nextjs'
 import { Inter } from "next/font/google";
 import { TopNav } from "./_components/TopNav";
 import "@uploadthing/react/styles.css";
+import { NextSSRPlugin } from "@uploadthing/react/next-ssr-plugin";
+import { extractRouterConfig } from "uploadthing/server";
+ 
+import { ourFileRouter } from "~/app/api/uploadthing/core";
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,6 +27,15 @@ export default function RootLayout({
   return (
     <ClerkProvider>
       <html lang="en">
+      <NextSSRPlugin
+          /**
+           * The `extractRouterConfig` will extract **only** the route configs
+           * from the router to prevent additional information from being
+           * leaked to the client. The data passed to the client is the same
+           * as if you were to fetch `/api/uploadthing` directly.
+           */
+          routerConfig={extractRouterConfig(ourFileRouter)}
+        />
         <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
           <TopNav />
           {children}
