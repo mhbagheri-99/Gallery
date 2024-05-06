e4db758d971029dedbf7e0e7fb9092b76df7cc23
mhbagheri-99 <mhbagheri3@gmail.com>
2024-05-06 18:21:58 +0200

    modify modal and image preview

--- src/app/@modal/(.)image/[id]/modal.tsx
+++ src/app/@modal/(.)image/[id]/modal.tsx
@@ -19,9 +19,9 @@ export function Modal({ children }: { children: React.ReactNode }) {
   }
 
   return createPortal(
-    <dialog ref={dialogRef} className="w-screen h-screen bg-black/80" onClose={onDismiss}>
+    <dialog ref={dialogRef} className="w-screen h-screen bg-black/80 text-white" onClose={onDismiss}>
       {children}
-      <button onClick={onDismiss} className="close-button" />
+      {/* <button onClick={onDismiss} className="close-button" /> */}
     </dialog>,
     document.getElementById('modal-root')!
   );
--- src/components/FullPageImageView.tsx
+++ src/components/FullPageImageView.tsx
@@ -3,6 +3,13 @@ import { getImage } from "~/server/queries";
 export default async function PhotoModal( { id }: { id: number }) {
   const image = await getImage(id);
   return (
-    <img src={image.url} alt={image.name} className="w-96"/>
+    <div className="flex w-full h-full min-w-0">
+      <div className="flex-shrink flex justify-center items-center">
+        <img src={image.url} alt={image.name} className="object-contain"/>
+      </div>
+      <div className="w-48 flex flex-col flex-shrink-0 border-l">
+        <div className="text-xl font-bold">{image.name}</div>
+      </div>
+    </div>
   );
 }
