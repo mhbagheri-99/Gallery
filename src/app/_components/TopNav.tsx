e9fdf50a05e9b657b3ff34b639cb62f4b7c99d88
mhbagheri-99 <mhbagheri3@gmail.com>
2024-05-06 19:23:56 +0200

    modify upload button style

--- src/app/_components/TopNav.tsx
+++ src/app/_components/TopNav.tsx
@@ -1,25 +1,20 @@
 'use client';
 
 import { SignInButton, SignedIn, SignedOut, UserButton } from "@clerk/nextjs";
-import { useRouter } from "next/navigation";
-import { UploadButton } from "~/utils/uploadthing";
+import { SimpleUploadButton } from "./SimpleUploadButton";
 
 export const TopNav = () => {
-  const router = useRouter();
 
   return (
   <nav className="bg-gray-800 flex items-center justify-between 
   p-4 border-b text-2xl font-semibold">
     <h1>Gallery</h1>
-    <div className="flex flex-row">
+    <div className="flex flex-row gap-4 items-center">
       <SignedOut>
         <SignInButton />
       </SignedOut>
       <SignedIn>
-        <UploadButton 
-          endpoint="imageUploader"
-          onClientUploadComplete={() => router.refresh()}
-        />
+        <SimpleUploadButton />
         <UserButton />
       </SignedIn>
     </div>
